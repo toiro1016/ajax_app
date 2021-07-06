5aa20386dfee91b145fa248bc1801f2a7c041e3e
toiro1016 <86334987+toiro1016@users.noreply.github.com>
2021-07-06 15:32:32 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,6 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(content: params[:content])
-    rrender json:{ post: post }
+    render json:{ post: post }
   end
 end
